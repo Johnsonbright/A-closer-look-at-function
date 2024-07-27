c5313c924a641e71b710bbadec1fab692f9efcb9
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-07-27 13:33:58 -0700

    more example on closure also using timeout

--- function.js
+++ function.js
@@ -25,19 +25,61 @@
 // }
 
 
-const secureBooking = function() {
-  let passengerCount = 0;
+// const secureBooking = function() {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`)
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`)
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker)
+
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function() {
+    console.log(a * 2);
   };
 };
 
-const booker = secureBooking();
+const h = function () {
+  const b = 10
+  f = function() {
+    console.log(b * 10);
+  };
+};
 
-booker();
-booker();
-booker();
+g();
+f();
+console.dir(f)
 
-console.dir(booker)
+// re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+
+const boardPassenger = function (n, wait) {
+  const person = n/3
+setTimeout(function () {
+  console.log(`We are now boarding all ${n} passengers `);
+  console.log(`There are 3 groups, each with ${person} passengers`);
+}, wait * 1000)
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// const person = 100
+boardPassenger(180, 5)
