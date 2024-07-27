88d8ebc496e9feedb190d1164a10bd1e82518735
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-07-27 09:45:11 -0700

    basic example of closure

--- function.js
+++ function.js
@@ -1,26 +1,43 @@
 "use strict "
-const flight = "LM235";
-const jonas = {
-  name: "Jonas Schmedtmann",
-  passport: 2434555632,
+// const flight = "LM235";
+// const jonas = {
+//   name: "Jonas Schmedtmann",
+//   passport: 2434555632,
+// };
+// const checkIn = function (flightNum, passenger) {
+// flightNum = 'LM999';
+// passenger.name ='Mr' + passenger.name;
+
+// if(passenger.passport === 2434555632) {
+//   alert("Check in")
+// }
+// else {
+//   alert("Wrong passport")
+// }
+// };
+
+// checkIn(flight, jonas)
+// console.log(flight)
+// console.log(jonas)
+
+// const newPassport = function(person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// }
+
+
+const secureBooking = function() {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`)
+  };
 };
-const checkIn = function (flightNum, passenger) {
-flightNum = 'LM999';
-passenger.name ='Mr' + passenger.name;
 
-if(passenger.passport === 2434555632) {
-  alert("Check in")
-}
-else {
-  alert("Wrong passport")
-}
-};
+const booker = secureBooking();
 
-checkIn(flight, jonas)
-console.log(flight)
-console.log(jonas)
+booker();
+booker();
+booker();
 
-const newPassport = function(person) {
-  person.passport = Math.trunc(Math.random() * 100000000000);
-}
-n
+console.dir(booker)
